3962b5db92ba9b213f166f318c7ca6fc892cf874
Aldric <aldric-ali@hotmail.fr>
2025-01-09 16:22:49 +0100

    search input

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Link href="/documents/50" className="hover:underline">Hello clique içi pour voir les documents</Link>
-    </div>
-  );
-}
