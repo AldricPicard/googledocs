1b642cb7239c9aa994ec6ef53d75875af9d2737f
Aldric <aldric-ali@hotmail.fr>
2025-01-10 22:32:31 +0100

    Templates gallery

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Navbar/>
     </div>
     <div className="mt-16">
-      <Link href="/documents/50" className="hover:underline">Hello clique içi pour voir les documents</Link>
+      <TemplatesGallery/>
     </div>
     </div>
   );
---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+    { 
+        id: "blank", 
+        Label: "Blank Document", 
+        imageUrl: "/blank-document.svg"
+    },
+    { 
+        id: "software-porposal", 
+        Label: "Software Proposal", 
+        imageUrl: "/software-proposal.svg"
+    },
+    { 
+        id: "project-proposal", 
+        Label: "Project Proposal", 
+        imageUrl: "/project-proposal.svg"
+    },
+    { 
+        id: "business-letter", 
+        Label: "Business Letter", 
+        imageUrl: "/business-letter.svg"
+    },
+    { 
+        id: "resume", 
+        Label: "Resume", 
+        imageUrl: "/resume.svg"
+    },
+    { 
+        id: "cover-letter", 
+        Label: "Cover Letter", 
+        imageUrl: "/cover-letter.svg"
+    },
+    { 
+        id: "letter", 
+        Label: "Letter", 
+        imageUrl: "/letter.svg"
+    },
+];
