1f6a982451ca5a7f2d297f9d27f230701d7f4782
Aldric <aldric-ali@hotmail.fr>
2025-01-13 16:26:42 +0100

    document menu

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button"
+import { ExternalLinkIcon, MoreVertical } from "lucide-react"
+import { Id } from "../../../convex/_generated/dataModel";
+import { 
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+
+
+interface DocumentMenuProps {
+    documentId: Id<"documents">;
+    title: string;
+    onNewTab: (id: Id<"documents">) => void;
+}
+
+export const DocumentMenu = ({documentId, title, onNewTab}: DocumentMenuProps) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="rounded-full">
+                    <MoreVertical className="size-4"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem 
+                    onClick={() => onNewTab(documentId)}
+                >
+                    <ExternalLinkIcon className="size-4 mr-2"/>
+                    Open in new tab
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -1,15 +1,22 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 import { SiGoogledocs } from "react-icons/si";
 import { Doc } from "../../../convex/_generated/dataModel";
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
 import { format } from "date-fns";
-import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
+import { cp } from "fs";
 
 interface DocumentRowProps {
     document: Doc<"documents">;
 };
 
 export const DocumentsRow = ({ document }: DocumentRowProps) => {
+
+    const onNewTabClick = (id: string) => {
+        window.open(`/documents/${id}`, "_blank");
+    }
+
+
     return (
         <TableRow className="cursor-pointer">
             <TableCell className="w-[50px]">
@@ -30,9 +37,11 @@ export const DocumentsRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "dd MMM yyyy")}
             </TableCell>
             <TableCell className="flex justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4"/>
-                </Button>
+                <DocumentMenu 
+                    documentId={document._id}
+                    title={document.title}
+                    onNewTab={onNewTabClick}
+                />
             </TableCell>
         </TableRow>
     )
